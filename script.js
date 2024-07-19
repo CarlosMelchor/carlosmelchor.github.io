1af0d345cc0b8069d43745d79aabc637db4cf68e
Carlos Melchor <Melchorcarlos02@gmail.com>
2024-07-19 23:41:23 +0800

    added project details and practicum details
    removed modals

--- script.js
+++ script.js
@@ -4,19 +4,3 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
-function openModal(modalId) {
-  document.getElementById(modalId).style.display = "block";
-}
-
-function closeModal(modalId) {
-  document.getElementById(modalId).style.display = "none";
-}
-
-window.onclick = function (event) {
-  const modals = document.querySelectorAll(".modal");
-  modals.forEach(modal => {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  });
-};
